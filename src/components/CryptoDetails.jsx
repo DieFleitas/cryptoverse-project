fee9e95c5e95e49ffe3faac6993d2aa3861e4c4e
DieFleitas <diegofleitas98@outlook.es>
2022-02-08 14:24:37 -0300

    layout

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CryptoDetails
+ = () => {
+    return ( 
+        <div>
+            CryptoDetails
+            
+        </div>
+     );
+}
+ 
+export default CryptoDetails
+;
