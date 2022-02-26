6f8c509be4fe6d28e0deb776b28a2df763601627
DieFleitas <diegofleitas98@outlook.es>
2022-02-25 21:13:32 -0300

    made CryptoDetails, cryptoApi, LineChart files

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,8 +15,25 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
-        })
-    })
-});
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+          }),
+      
+          getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+          }),
+      
+          // Note: To access this endpoint it require premium plan
+          getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+          }),
+        }),
+      });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const {
+        useGetCryptosQuery,
+        useGetCryptoDetailsQuery,
+        useGetExchangesQuery,
+        useGetCryptoHistoryQuery,
+      } = cryptoApi;
